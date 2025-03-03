2ea09212f52aec1d219f872f8d3449116eb84cf9
PoshalaAmulya <125126547+PoshalaAmulya@users.noreply.github.com>
2025-03-03 21:10:59 +0530

    Time: 1 ms (41.66%) | Memory: 41 MB (46.90%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         int c=0;
-        if(n==0) return false;
+        if(n<=0) return false;
         while(n>0){
             int bit=(n&1);
             if(bit!=0) c++;
             n=(n>>1);
             if(c>1) return false;
         }
-        if(c==1)
+        
         return true;
-        return false;
+       
     }
 }
